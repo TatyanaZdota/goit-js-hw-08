c82e0cd16cbca040f59105cc5934f47dff72c960
TatyanaZdota <99191661+TatyanaZdota@users.noreply.github.com>
2023-11-21 19:59:44 +0200

    add second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeEl);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+
+const savedTime = function (data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+};
+const throttleSavedTime = throttle(savedTime, 1000);
+player.on('timeupdate', throttleSavedTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,7 @@
 import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
-const storageKey = 'feedback-form-state';
+const STORAGE_KEY = 'feedback-form-state';
 const formData = {};
 
 formEl.addEventListener('input', throttle(onFormInput), 500);
@@ -9,27 +9,23 @@ formEl.addEventListener('submit', onFormSubmit);
 
 function onFormInput(event) {
   formData[event.target.name] = event.target.value;
-  localStorage.setItem(storageKey, JSON.stringify(formData));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function onFormSubmit(event) {
   event.preventDefault();
-  const { email, message } = event.currentTarget.elements;
 
+  const { email, message } = event.currentTarget.elements;
   if (email.value.length < 1 || message.value.length < 1) {
     alert('All fields must be filled!');
   }
-  const data = {
-    email: email.value,
-    message: message.value,
-  };
-  console.log(data);
+  console.log(formData);
   event.currentTarget.reset();
-  localStorage.removeItem(storageKey);
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 function localText() {
-  const savedMessage = localStorage.getItem(storageKey);
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
   if (savedMessage) {
     const dataParse = JSON.parse(savedMessage);
     formData.email = dataParse.email || '';
